71cf00c675bcf2f0f4294883d836a5015e5e964b
Félix Saparelli <felix@passcod.name>
2017-06-02 13:53:42 +1200

    Hello shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Stalc {}", env!("CARGO_PKG_VERSION"));
+
+    loop {
+        print!("> ");
+        stdout().flush().unwrap_or(());
+
+        let mut input = String::new();
+        match stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{} bytes read", n);
+                println!("{}", input);
+            }
+            Err(error) => println!("error: {}", error),
+        }
+    }
 }
